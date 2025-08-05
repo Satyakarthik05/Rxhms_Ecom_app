43d7ca196803f07328e08c9c0fb0818e3b10fcc8
Harika Chittiprolu <hchittiprolu@chssoftwares.com>
2025-08-05 09:52:34 +0530

    Created Folder structure, installed packages and Implemented Custom fonts and vector-icons set up

---
+++ src/screens/OtpVerifyScreen/index.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const OtpVerifyScreen = () => {
+  return (
+    <View>
+      <Text>OtpVerifyScreen</Text>
+    </View>
+  )
+}
+
+export default OtpVerifyScreen
+
+const styles = StyleSheet.create({})
